94f3c5c68f407fd037a256d05283d0d1a8ba1535
Muhammad Khan <muhammadkh4n@gmail.com>
2018-05-25 14:50:24 +0500

    replace all the urls in msg body with anchor tags

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
-const urlRe = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+const urlRe = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
 
 export const isUrl = value => {
   return urlRe.test(value);
